88b1acc7fed1c58aa110c86547cb73062b3d5ab4
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-02-16 23:24:46 +0100

    feat(android): simplified the example, added subactivity

--- app/src/main/java/io/baff/sample/SubActivity.java
+++ app/src/main/java/io/baff/sample/SubActivity.java
@@ -1,27 +1,20 @@
 package io.baff.sample;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import io.baff.core.GetBaff;
 
-public class MainActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
     }
 
     public void onRunGetBaff(View view) {
         GetBaff.ShowGetBaff(this);
-    }
-
-    public void onStartSub(View view) {
-        Intent intent = new Intent(this, SubActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-        this.startActivity(intent);
     }
 }
